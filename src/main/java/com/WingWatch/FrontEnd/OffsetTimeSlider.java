071912f64a126e1aeaf01bf30868747d54ddcd24
wyu4 <wilsonyu657@gmail.com>
2024-12-12 13:16:01 -0500

    Adjust step forward/backward size

--- src/main/java/com/WingWatch/FrontEnd/OffsetTimeSlider.java
+++ src/main/java/com/WingWatch/FrontEnd/OffsetTimeSlider.java
@@ -10,8 +10,8 @@ import java.time.ZonedDateTime;
 
 public class OffsetTimeSlider extends JPanel implements ActionListener {
     private final JSlider slider = new JSlider();
-    private final JLabel timeLabel = new JLabel("+/-00:00:00:00");
-    private final JButton stepBackwards = new JButton("<"), resetButton = new JButton("0"), stepForward = new JButton(">");
+    private final JLabel timeLabel = new JLabel("+/-" + SkyClock.formatTimeLeft(0L));
+    private final JButton stepBackwards = new JButton("-10"), resetButton = new JButton("0"), stepForward = new JButton("+10");
 
     public OffsetTimeSlider() {
         setName("TimeSlider");
@@ -35,35 +35,34 @@ public class OffsetTimeSlider extends JPanel implements ActionListener {
 
         timeLabel.setFont(timeLabel.getFont().deriveFont(Font.BOLD));
 
+        Insets defaultInsets = new Insets(0, (int)(App.SCREEN_SIZE.width*0.005), 0, 0);
         GridBagConstraints constraints = new GridBagConstraints();
         constraints.fill = GridBagConstraints.HORIZONTAL;
-        constraints.weightx = 2;
+        constraints.weightx = 1;
         constraints.gridx = 1; constraints.gridy = 1;
-        constraints.insets = new Insets(0, (int)(App.SCREEN_SIZE.width*0.005), 0, 0);
+        constraints.insets = defaultInsets;
         add(slider, constraints);
 
         constraints.gridx = 2; constraints.gridy = 1;
-        constraints.weightx = 0;
-        constraints.insets = new Insets(0, constraints.insets.left, 0, constraints.insets.left);
         add(timeLabel, constraints);
 
         constraints.gridx = 3; constraints.gridy = 1;
-        constraints.insets = new Insets(0, constraints.insets.left, 0, constraints.insets.left);
+        constraints.insets = new Insets(0, defaultInsets.left, 0, 0);
         add(stepBackwards, constraints);
 
         constraints.gridx = 4; constraints.gridy = 1;
-        constraints.insets = new Insets(0, constraints.insets.left, 0, constraints.insets.left);
+        constraints.insets = new Insets(0, 0, 0, 0);
         add(resetButton, constraints);
 
         constraints.gridx = 5; constraints.gridy = 1;
-        constraints.insets = new Insets(0, constraints.insets.left, 0, constraints.insets.left);
+        constraints.insets = new Insets(0, 0, 0, defaultInsets.left);
         add(stepForward, constraints);
     }
 
     public ZonedDateTime getOffset(ZonedDateTime time) {
         int offsetSeconds = slider.getValue();
         if (offsetSeconds == 0) {
-            timeLabel.setText("+/-00:00:00:00");
+            timeLabel.setText("+/-" + SkyClock.formatTimeLeft(0L));
             resetButton.setVisible(false);
         } else {
             resetButton.setVisible(true);
@@ -84,9 +83,9 @@ public class OffsetTimeSlider extends JPanel implements ActionListener {
         if (e.getSource().equals(resetButton)) {
             slider.setValue(0);
         } else if (e.getSource().equals(stepForward)) {
-            step(1);
+            step(10);
         } else if (e.getSource().equals(stepBackwards)) {
-            step(-1);
+            step(-10);
         }
     }
 }
