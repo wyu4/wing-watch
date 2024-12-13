8add96261b005f70a9863b522995260ee79aff89
wyu4 <wilsonyu657@gmail.com>
2024-12-13 06:27:45 -0500

    Add Try/Catch to error file creation not working.

--- src/main/java/com/WingWatch/Start.java
+++ src/main/java/com/WingWatch/Start.java
@@ -13,12 +13,15 @@ import java.net.URISyntaxException;
 
 public class Start {
     public static void main(String[] args) throws IOException, URISyntaxException {
-        File errors = new File("Errors.txt");
-        errors.createNewFile();
-        System.setErr(new PrintStream(errors));
+        try {
+            File errors = new File("Errors.txt");
+            errors.createNewFile();
+            System.setErr(new PrintStream(errors));
+        } catch (IOException e) {
+            System.err.println("Could not create error file: " + e.getMessage());
+        }
 
         try {
-            SkyClockUtils.refreshData();
             FlatLaf.registerCustomDefaultsSource("themes");
             WingWatchDark.setup();
             EventQueue.invokeLater(App::new);
